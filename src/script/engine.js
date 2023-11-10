17b4c0ef469f876aa2bb27e2d48844efd6694229
Delcio Brito <delciobrito82@gmail.com>
2023-11-10 01:07:23 -0300

    Desenvolvendo a engine.js

---
+++ src/script/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+    "😄",
+    "😄",
+    "💯",
+    "💯",
+    "👽",
+    "👽",
+    "🏦",
+    "🏦",
+    "🗡️",
+    "🗡️",
+    "🐒",
+    "🐒",
+    "🐱",
+    "🐱",
+    "🌴",
+    "🌴"
+]
+let openCards = []
+console.log(emojis)
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
+
+for(let i = 0; i > emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box)
+}
