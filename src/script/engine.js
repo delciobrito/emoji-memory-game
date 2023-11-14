7fe94c82c6d92fc8f3cb68205a1b6584557fd5fd
Delcio Brito <delciobrito82@gmail.com>
2023-11-14 00:28:52 -0300

    Dando efeito de rotate aos cards

--- src/script/engine.js
+++ src/script/engine.js
@@ -24,5 +24,20 @@ for(let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleclick
     document.querySelector(".game").appendChild(box)
 }
+
+function handleclick() {
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen")
+        openCards.push(this)
+        console.log(openCards)
+    }
+
+    if(openCards == 2) {
+        setTimeout(checkMath, 900)
+    }
+}
+
+function checkMath() {}
