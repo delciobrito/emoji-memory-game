a82539cb22d461205578d1a2bae81fa29216eb50
Delcio Brito <delciobrito82@gmail.com>
2023-11-13 21:08:15 -0300

    Terminando os efeitos da engine.js

--- src/script/engine.js
+++ src/script/engine.js
@@ -17,10 +17,10 @@ const emojis = [
     "🌴"
 ]
 let openCards = []
-console.log(emojis)
+
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1))
 
-for(let i = 0; i > emojis.length; i++) {
+for(let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
